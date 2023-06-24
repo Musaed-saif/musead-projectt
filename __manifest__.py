f9575ed3a151cded5f6310ec5daf107d18747895
Musaed-saif <137612238+Musaed-saif@users.noreply.github.com>
2023-06-24 14:42:18 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+  'name': 'Car project',
+  'version': '1.0',
+  'category': 'Accounting/Accounting',
+  'author': 'musaed saif amer',
+  'website': "",
+  'company': 'musaed saif amer',
+  'maintainer': 'musaed saif amer',
+  'summary': 'Record Car Details',
+  "description": "",
+  'depends': ['base'],
+   'data': [
+      'security/car_project.csv',
+      'views/car_prject_views.xml',
+  ],
+  'installable':True,
+  'application':True,
+  'license':'LGPL-3',
+ 
+}
